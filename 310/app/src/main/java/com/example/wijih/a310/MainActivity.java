7fd6a493b65df023b9d29b58cb175abb42344746
Namita Prakash <namitapr@usc.edu>
2018-10-20 20:05:19 -0700

    Made swiping work

--- 310/app/src/main/java/com/example/wijih/a310/MainActivity.java
+++ 310/app/src/main/java/com/example/wijih/a310/MainActivity.java
@@ -5,17 +5,22 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.Toast;
+import android.view.View;
 
 import com.lorentzos.flingswipe.SwipeFlingAdapterView;
 
 import java.util.ArrayList;
 
 import butterknife.ButterKnife;
+import butterknife.BindView;
+import butterknife.OnClick;
 
 public class MainActivity extends Activity {
     private ArrayList<String> al;
     private ArrayAdapter<String> arrayAdapter;
     private int i;
+
+    @BindView(R.id.frame) SwipeFlingAdapterView flingContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +42,7 @@ public class MainActivity extends Activity {
         arrayAdapter = new ArrayAdapter<>(this, R.layout.item, R.id.helloText, al );
 
 
-        SwipeFlingAdapterView flingContainer = (SwipeFlingAdapterView) findViewById(R.id.frame);
+        flingContainer.setAdapter(arrayAdapter);
         flingContainer.setFlingListener(new SwipeFlingAdapterView.onFlingListener() {
             @Override
             public void removeFirstObjectInAdapter() {
@@ -52,12 +57,12 @@ public class MainActivity extends Activity {
                 //Do something on the left!
                 //You also have access to the original object.
                 //If you want to use it just cast it (String) dataObject
-                Toast.makeText(MainActivity.this, "Liked!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "Disliked!", Toast.LENGTH_SHORT).show();
             }
 
             @Override
             public void onRightCardExit(Object dataObject) {
-                Toast.makeText(MainActivity.this, "Disliked!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "Liked!", Toast.LENGTH_SHORT).show();
             }
 
             @Override
@@ -71,17 +76,29 @@ public class MainActivity extends Activity {
 
             @Override
             public void onScroll(float scrollProgressPercent) {
+                View view = flingContainer.getSelectedView();
+                view.findViewById(R.id.item_swipe_right_indicator).setAlpha(scrollProgressPercent < 0 ? -scrollProgressPercent : 0);
+                view.findViewById(R.id.item_swipe_left_indicator).setAlpha(scrollProgressPercent > 0 ? scrollProgressPercent : 0);
             }
         });
 
-
-        // Optionally add an OnItemClickListener
+        // A user can tap on a book profile to view additional details on the book
         flingContainer.setOnItemClickListener(new SwipeFlingAdapterView.OnItemClickListener() {
             @Override
             public void onItemClicked(int itemPosition, Object dataObject) {
-                Toast.makeText(MainActivity.this, "click", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "Additional book info will go here...", Toast.LENGTH_SHORT).show();
             }
         });
 
     }
+
+//    @OnClick(R.id.right)
+//    public void right() {
+//        flingContainer.getTopCardListener().selectRight();
+//    }
+//
+//    @OnClick(R.id.left)
+//    public void left() {
+//        flingContainer.getTopCardListener().selectLeft();
+//    }
 }
