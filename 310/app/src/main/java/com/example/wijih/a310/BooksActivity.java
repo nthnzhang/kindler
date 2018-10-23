4c35613474eadb838c87eaa710ffab844cfb5693
Nathan Zhang <nathanzh@usc.edu>
2018-10-22 23:45:34 -0700

    attempt to make the show book function async

--- 310/app/src/main/java/com/example/wijih/a310/BooksActivity.java
+++ 310/app/src/main/java/com/example/wijih/a310/BooksActivity.java
@@ -1,13 +1,24 @@
 package com.example.wijih.a310;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import com.example.wijih.a310.model.User;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,22 +29,47 @@ public class BooksActivity extends AppCompatActivity {
     //List<String> books = user.getBooks();
     List<String> books = new ArrayList<String>();
 
+    private User currentUser;
+    private DatabaseReference mDatabase;
+
+    private ArrayAdapter<String> adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_books);
 
-        books.add("harry potter");
-        books.add("Hunger games");
-        books.add("friends");
-        books.add("the office");
-        books.add("hello");
+        final Intent currUserIntent = getIntent();
+        currentUser = currUserIntent.getParcelableExtra("current_user");
 
 
         verticalBooks = (ListView) findViewById(R.id.verticalBooksView);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.activity_books_view, R.id.booksDisplay, books);
+        adapter = new ArrayAdapter<String>(this, R.layout.activity_books_view, R.id.booksDisplay, books);
         verticalBooks.setAdapter(adapter);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("users").child(currentUser.getUserID()).child("uploadedBookIDs");
+
+        mDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for(DataSnapshot userSnapshot : dataSnapshot.getChildren()) {
+                    // getting each bookId
+                    Log.d("test", userSnapshot.getValue(String.class));
+                    books.add(userSnapshot.getValue(String.class));
+                    adapter.notifyDataSetChanged();
+                }
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
+
     }
 
 }
