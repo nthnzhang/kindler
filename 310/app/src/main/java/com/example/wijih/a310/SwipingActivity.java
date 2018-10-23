c84620cc6b8188bbf3d1cff770316b7923e0ae17
Nathan Zhang <nathanzh@usc.edu>
2018-10-22 18:03:53 -0700

    fix merge conflict

--- 310/app/src/main/java/com/example/wijih/a310/SwipingActivity.java
+++ 310/app/src/main/java/com/example/wijih/a310/SwipingActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.wijih.a310.model.Book;
