7478dbdf9f6cd473f0c35d25381ea3f2ede8628b
Namita Prakash <namitapr@usc.edu>
2018-10-29 23:10:03 -0700

    Implemented more black box test cases for logging in and registering as a new user

--- 310/app/src/androidTest/java/com/example/wijih/a310/LoginActivityUITest.java
+++ 310/app/src/androidTest/java/com/example/wijih/a310/LoginActivityUITest.java
@@ -13,6 +13,7 @@ import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
@@ -21,9 +22,9 @@ import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
-public class ChooseLoginRegistrationActivityUITest {
+public class LoginActivityUITest {
     @Rule
-    public ActivityTestRule<ChooseLoginRegistrationActivity> mActivityRule = new ActivityTestRule<>(ChooseLoginRegistrationActivity.class);
+    public ActivityTestRule<LoginActivity> mActivityRule = new ActivityTestRule<>(LoginActivity.class);
 
     @Before
     public void setUp() throws Exception {
@@ -31,28 +32,23 @@ public class ChooseLoginRegistrationActivityUITest {
     }
 
     @Test
-    public void testLoginButton() {
-        onView(withText("LOGIN"))
-                .perform(click());
-
-        // Verify that we have really clicked on the icon by checking the TextView content.
-        onView(withId(R.id.email)).check(matches(isDisplayed()));
-        onView(withId(R.id.password)).check(matches(isDisplayed()));
-        onView(withId(R.id.login)).check(matches(isDisplayed()));
+    public void testLoginAbility() {
+        onView(withId(R.id.email)).perform(typeText("avalidemail@email.com"));
+        onView(withId(R.id.password)).perform(typeText("validpassword"));
+        onView(withId(R.id.login)).perform(click());
+        onView(withId(R.id.frame)).check(matches(isDisplayed()));
     }
 
     @Test
-    public void testRegisterButton() {
-        onView(withText("REGISTER"))
-                .perform(click());
-
-        // Verify that we have really clicked on the icon by checking the TextView content.
-        onView(withId(R.id.number)).check(matches(isDisplayed()));
+    public void testLoginBlock() {
+        onView(withId(R.id.email)).perform(typeText("notavalidemail@email.com"));
+        onView(withId(R.id.password)).perform(typeText("notavalidpassword"));
+        onView(withId(R.id.login)).perform(click());
+        onView(withId(R.id.login)).check(matches(isDisplayed()));
     }
 
     @After
     public void tearDown() throws Exception {
 
     }
-
 }
--- 310/app/src/androidTest/java/com/example/wijih/a310/RegistrationActivityUITest.java
+++ 310/app/src/androidTest/java/com/example/wijih/a310/RegistrationActivityUITest.java
@@ -13,6 +13,7 @@ import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
@@ -21,9 +22,9 @@ import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
-public class ChooseLoginRegistrationActivityUITest {
+public class RegistrationActivityUITest {
     @Rule
-    public ActivityTestRule<ChooseLoginRegistrationActivity> mActivityRule = new ActivityTestRule<>(ChooseLoginRegistrationActivity.class);
+    public ActivityTestRule<RegistrationActivity> mActivityRule = new ActivityTestRule<>(RegistrationActivity.class);
 
     @Before
     public void setUp() throws Exception {
@@ -31,28 +32,17 @@ public class ChooseLoginRegistrationActivityUITest {
     }
 
     @Test
-    public void testLoginButton() {
-        onView(withText("LOGIN"))
-                .perform(click());
-
-        // Verify that we have really clicked on the icon by checking the TextView content.
-        onView(withId(R.id.email)).check(matches(isDisplayed()));
-        onView(withId(R.id.password)).check(matches(isDisplayed()));
-        onView(withId(R.id.login)).check(matches(isDisplayed()));
-    }
-
-    @Test
-    public void testRegisterButton() {
-        onView(withText("REGISTER"))
-                .perform(click());
-
-        // Verify that we have really clicked on the icon by checking the TextView content.
-        onView(withId(R.id.number)).check(matches(isDisplayed()));
+    public void testRegistrationBlockingAbility() {
+        onView(withId(R.id.email)).perform(typeText("avalidemail@email.com"));
+        onView(withId(R.id.username)).perform(typeText("thisuseralreadyexists"));
+        onView(withId(R.id.number)).perform(typeText("123456"));
+        onView(withId(R.id.password)).perform(typeText("notactuallyvalid!"));
+        onView(withId(R.id.register)).perform(click());
+        onView(withId(R.id.register)).check(matches(isDisplayed()));
     }
 
     @After
     public void tearDown() throws Exception {
 
     }
-
 }
